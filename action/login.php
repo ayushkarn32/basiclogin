3ebd727bb38c5017c855a6e8537052f757978d4c
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2020-07-30 03:00:24 +0545

    Update login.php

--- action/login.php
+++ action/login.php
@@ -1,33 +1,42 @@
 <?php
- 
-$user=$_POST['user'];; //change it as your post name
-$pass=$_POST['pass'];;   //change it as your post name
- 
+session_start();
+$email1=$_POST['email'];
+$email=md5($email1);
+$passwd1=$_POST['password'];
+$passwd=md5($passwd1);
+
 //conect to database
 $servername = "localhost";
 $username = "root";
 $password = "";
 $db = "login";
-$conn = new mysqli($servername, $username, $password,$db);
+$conn = mysqli_connect($servername, $username, $password,$db);
  
-//checking connection  
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if($conn){
+//echo "connected";
+}else{
+die("error in connection");
 }
- //getting users value from users table
-$query = "SELECT * FROM `users` WHERE username='$user' and password='$pass'";
+
+
+$query = "SELECT * FROM `users` WHERE email = '$email' and passwd='$passwd' ";
     $result = mysqli_query($conn,$query) or die(mysql_error());
     $row = $result->fetch_assoc();
    
 //check for the resulted quesry
-if($row['username']==$user && $row['password']==$pass)
+if($row['email']==$email && $row['passwd']==$passwd)
    
 {
-    echo "Login Success Welcome ".$row['username'];
-	header( "refresh:1;url=home.php" );
+    echo "Login Success Welcome ".$row['fname'];
+	$_SESSION['email'] = $email;
+	header( "refresh:1;url=pages/home.php" );
 	}
 else
 {
     echo "Failed to login!!";
-	header( "refresh:1;url=login.php?user=$user" );
 }
+
+
+
+    
+?>
