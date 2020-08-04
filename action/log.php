26c0af64640607dbea217d2f738099099b6bc71c
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2020-08-04 14:17:46 +0545

    Update log.php

--- action/log.php
+++ action/log.php
@@ -1,3 +1,23 @@
 <?php
+      session_start();
+      
+      $ip = "127.0.0.1";     
+		  $servername = "localhost";
+			$username = "root";
+			$password = "";
+			$db = "login";
+			$conn = mysqli_connect($servername, $username, $password,$db);
+         
+             $email1="abc@xyz.com";             //Fetch userid and password from login form  
 
+	$query = "INSERT INTO iplogger VALUES ('','$email1','$ip','')";
+	$result = mysqli_query($conn,$query);
+    if($result)
+    {
+    	header( "refresh:0;url=index.html" );
+    }
+    else {
+    	echo "some error occured please login again" ;
+    	//header( "refresh:1;url=in.html" );
+    }
 ?>
