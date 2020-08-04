775e5d03e88367a18e8565f6e86b3f5f917a9e43
Aayush Karna <33031838+ayushkarn32@users.noreply.github.com>
2020-08-04 14:36:57 +0545

    Update log.php

--- action/log.php
+++ action/log.php
@@ -1,17 +1,25 @@
 <?php
       session_start();
       
-      $ip = "127.0.0.1";     
-		  $servername = "localhost";
-			$username = "root";
-			$password = "";
-			$db = "login";
-			$conn = mysqli_connect($servername, $username, $password,$db);
-         
-             $email1="abc@xyz.com";             //Fetch userid and password from login form  
+      $ip =$_SERVER['REMOTE_ADDR'];
+      $email1=$_SESSION["email"];
 
-	$query = "INSERT INTO iplogger VALUES ('','$email1','$ip','')";
+	//database connection
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $db = "login";
+      $conn = mysqli_connect($servername, $username, $password,$db);
+         
+	//inserting the users logged value
+
+//here i haven't defined $id because id is primary key in the database and is automatically incremented
+// $date is the currenttimestamp which means whenever ip is logged the date and 
+	
+	$query = "INSERT INTO iplogger VALUES ('$id','$email1','$ip','$date')";
 	$result = mysqli_query($conn,$query);
+    
+//if logged sucessfully you will be redirected to 
     if($result)
     {
     	header( "refresh:0;url=index.html" );
